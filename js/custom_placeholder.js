b2ca8a2e8b549f440a25bd47a16dec737504cc30
eldhose <eldhose104@gmail.com>
2015-07-24 14:37:48 +0530

    created left pane

--- js/custom_placeholder.js
+++ js/custom_placeholder.js
@@ -62,3 +62,19 @@ $(function(){
         $("#upload:hidden").trigger('click');
     });
 });
+
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+    } else {
+        $('#sticky').removeClass('stick');
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
